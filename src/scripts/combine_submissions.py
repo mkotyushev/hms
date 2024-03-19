6a744d97f80682d76270169cb99d7d214cefb2fa
Mikhail Kotyushev <mkotyushev@gmail.com>
2024-03-19 02:17:11 +0000

    Make submission combine script more verbose

--- src/scripts/combine_submissions.py
+++ src/scripts/combine_submissions.py
@@ -1,8 +1,13 @@
 import argparse
+import logging
 import pandas as pd
 from pathlib import Path
 
 from src.data.constants import LABEL_COLS_ORDERED
+
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
 
 
 def parse_args():
@@ -13,13 +18,25 @@ def parse_args():
 
 
 def main(args):
+    logger.info(f'Combining submissions: {args.submission_filepathes}')
     dfs = [pd.read_csv(filepath) for filepath in args.submission_filepathes]
     assert all([df.columns.tolist() == dfs[0].columns.tolist() for df in dfs])
     assert all([(df['eeg_id'] == dfs[0]['eeg_id']).all() for df in dfs])
     
+    for filepath, df in zip(args.submission_filepathes, dfs):
+        logger.info(
+            f'Loaded submission from {filepath} with '
+            f'shape: {df.shape}, '
+            f'columns: {df.columns.tolist()}'
+        )
+        logger.info(f'First rows:\n{df.head()}')
+
     # Mean
     df = dfs[0].copy()
     df[LABEL_COLS_ORDERED] = sum([d[LABEL_COLS_ORDERED].values for d in dfs]) / len(dfs)
+
+    logger.info(f'Combined submission with shape: {df.shape}, columns: {df.columns.tolist()}')
+    logger.info(f'First rows:\n{df.head()}')
 
     # Write
     if args.output:
