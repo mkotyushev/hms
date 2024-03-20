dd16a37b944e52f28b06bf557070b9db3add18d1
Mikhail Kotyushev <mkotyushev@gmail.com>
2024-03-20 12:47:25 +0000

    Remove affine transforms from augs

--- src/data/transforms.py
+++ src/data/transforms.py
@@ -2,12 +2,13 @@ import cv2
 import math
 import numpy as np
 import random
+import torch
 from copy import deepcopy
 from justpyplot import justpyplot as jplt
 from PIL import Image
 from scipy.signal import butter, lfilter
 from torchvision.transforms import TrivialAugmentWide
-from typing import Dict, List, Callable
+from typing import Dict, List, Callable, Tuple
 
 from .constants import (
     SPECTROGRAM_N_SAMPLES, 
@@ -345,6 +346,25 @@ class TrivialAugmentWideWrapper(TrivialAugmentWide):
         img = np.array(img) / 255.0
         item['image'] = img
         return item
+
+    def _augmentation_space(self, num_bins: int) -> Dict[str, Tuple[torch.Tensor, bool]]:
+        return {
+            # op_name: (magnitudes, signed)
+            "Identity": (torch.tensor(0.0), False),
+            # "ShearX": (torch.linspace(0.0, 0.99, num_bins), True),
+            # "ShearY": (torch.linspace(0.0, 0.99, num_bins), True),
+            # "TranslateX": (torch.linspace(0.0, 32.0, num_bins), True),
+            # "TranslateY": (torch.linspace(0.0, 32.0, num_bins), True),
+            # "Rotate": (torch.linspace(0.0, 135.0, num_bins), True),
+            "Brightness": (torch.linspace(0.0, 0.99, num_bins), True),
+            "Color": (torch.linspace(0.0, 0.99, num_bins), True),
+            "Contrast": (torch.linspace(0.0, 0.99, num_bins), True),
+            "Sharpness": (torch.linspace(0.0, 0.99, num_bins), True),
+            "Posterize": (8 - (torch.arange(num_bins) / ((num_bins - 1) / 6)).round().int(), False),
+            "Solarize": (torch.linspace(255.0, 0.0, num_bins), False),
+            "AutoContrast": (torch.tensor(0.0), False),
+            "Equalize": (torch.tensor(0.0), False),
+        }
 
 
 class To01:
