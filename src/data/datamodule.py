0a429ca51c1152d22edbd10573d27b1f1fd42714
Mikhail Kotyushev <mkotyushev@gmail.com>
2024-03-22 04:37:01 +0000

    Add resize

--- src/data/datamodule.py
+++ src/data/datamodule.py
@@ -47,6 +47,7 @@ class HmsDatamodule(LightningDataModule):
         label_smoothing_n_voters: int | None = None,
         low_n_voters_strategy: Literal['keep', 'pl'] | None = None,
         by_subrecord: bool = False,
+        img_size: int = 640,
         cache_dir: Optional[Path] = None,
         batch_size: int = 32,
         num_workers: int = 0,
@@ -98,6 +99,7 @@ class HmsDatamodule(LightningDataModule):
                     mask_fill_value=0, 
                     p=0.5,
                 ),
+                A.Resize(self.hparams.img_size, self.hparams.img_size),
                 Unsqueeze(),
             ]
         )
@@ -108,6 +110,7 @@ class HmsDatamodule(LightningDataModule):
                     eps=1e-6
                 ),
                 ToImage(),
+                A.Resize(self.hparams.img_size, self.hparams.img_size),
                 Unsqueeze(),
             ]
         )
