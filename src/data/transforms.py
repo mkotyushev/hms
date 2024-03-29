a0f1a0a32c82ee120ff3b9ac4bf526e23b3c1002
Mikhail Kotyushev <mkotyushev@gmail.com>
2024-03-29 03:31:15 +0000

    Change image size and composition

--- src/data/transforms.py
+++ src/data/transforms.py
@@ -347,7 +347,7 @@ def butter_lowpass_filter(data, cutoff=20, fs=EED_SAMPLING_RATE_HZ, order=5):
 class ToImage:
     def __call__(self, *args, force_apply: bool = False, **item):
         # TODO: add adaptive image size
-        img_array = np.zeros((16 * len(EEG_DIFF_COL_INDICES), 640, 4), dtype=np.uint16)
+        img_array = np.zeros((16 * len(EEG_DIFF_COL_INDICES), 1600, 4), dtype=np.uint16)
 
         eeg = item['eeg']
         subdf = eeg[3000:7000].T
@@ -381,20 +381,20 @@ class ToImage:
                 plot_to_array(y, img_array[16 * i - 8:16 * i + 16 + 8, :])
 
         img_array = np.clip(img_array, 0, 255)
-        img = np.zeros((640, 640), dtype=np.float32)
-        img[320:, :] = img_array[..., 3] / 255.0
+        img = np.zeros((320, 640+1600), dtype=np.float32)
+        img[:, 640:] = img_array[..., 3] / 255.0
 
         # 10 minutes spectrogram
         y = item['spectrogram']
         y[np.isnan(y)] = 0
         y = cv2.resize(y, (320, 320))
-        img[:320, 320:] = y
+        img[:, 320:640] = y
 
         # 50 seconds EEG spectrogram
         y = item['eeg_spectrogram']
         y = y.transpose(1, 2, 0).reshape(-1, y.shape[0] * y.shape[2])
         y = cv2.resize(y, (320, 320))
-        img[:320, :320] = y
+        img[:, :320] = y
     
         item['image'] = img
         return item
